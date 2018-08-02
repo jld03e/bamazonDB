cb0add5067d4a2db1c2a00d5360052fa2322f086
Jose De Las Salas <SueBuSo@Joses-MacBook-Pro.local>
2018-08-02 18:14:25 -0400

    added code that i had worked on a seperate test file for the JS portion of hw assignment, created DB file for the bamazon app, currently stuck with trying to confirm the reduction of quantities.

---
+++ bamazonDB.js
@@ -0,0 +1,155 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Carrots072087",
+  database: "bamazonDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  runProductGuide()
+});
+
+function runProductGuide() {
+  inquirer
+  .prompt({
+    name: "action",
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+      "View Product List",
+      "Buy units of product using ID information"
+    ]
+  })
+  .then(function(answer) {
+    console.log(answer);
+    switch(answer.action) {
+      case "View Product List":
+      productSearch();
+      break;
+
+      case "Buy units of product using ID information":
+      buyUnits();
+      break;
+    }
+  });
+}
+
+function productSearch () {
+  // inquirer
+  // .prompt({
+  //   name: "id",
+  //   type: "input",
+  //   message: "What's the ID for the product you want to buy?"
+  // })
+  // .then(function(answer) 
+    var query = "SELECT * FROM products";
+    connection.query(query, {
+    }, function(err, res) {
+      for (var i=0; i<res.length; i++) {
+      console.log("Item ID: " + res[i].item_id +
+                  " == Product Name: " + res[i].product_name + 
+                  " == Quantities Left: " + res[i].stock_quantity);
+      }
+      buyUnits();
+    })
+}
+
+
+function buyUnits() {
+  connection.query("SELECT * FROM products", function(err, results){
+    console.log(results);
+    if (err) throw err;
+      var choiceArray = [];
+      for (var i=0; i<results.length; i++) {
+        choiceArray.push(results[i].item_id);
+      }
+      console.log(choiceArray);
+    inquirer
+    .prompt([
+      {
+        name: "choice",
+        type: "input",
+        message: "Which item would you like to purchase?"
+      },
+      {
+        name: "units",
+        type: "input",
+        message: "How many units would you like to purchase?"
+      }
+    ])
+    .then(function(answer) {
+      for (var j = 0; j < results.length; j++) {
+        if (parseInt(results[j].item_id) === parseInt(answer.choice))
+        console.log(results[j]);
+        var remainder = parseInt(results[j].stock_quantity) - parseInt(answer.units);
+        console.log(remainder);
+      }
+      
+      //determine if there are any quantities left:
+      if (parseInt(remainder) > 0) {
+        connection.query(
+          "UPDATE products SET ? WHERE ?",
+          [
+            {
+              stock_quantity: remainder
+            },
+            {
+              item_id: results[j].item_id
+            }
+          ],
+          function(error) {
+            if (error) throw err;
+            console.log("ENJOY YOUR PURCHASE!");
+            runProductGuide();
+          }
+        );
+      }
+      else {
+          console.log("Not enough units! Revise purchase!");
+          runProductGuide();
+      }
+      //this closes the else.
+    });
+      });
+    };
+
+// function buyUnits() {
+//   inquirer
+//   .prompt([
+//     {
+//     name: "purchase",
+//     type: "input",
+//     message: "What's the ID for the product you wish to purchase?",
+//     validate: function(value) {
+//       if (isNaN(value) === false) {
+//         return true;
+//       }
+//       return false;
+//     }
+//   }
+//   ])
+//   .then(function(answer) {
+//     var query = "SELECT item_id FROM bamazonDB WHERE ?";
+//     connection.query(query, 
+//       {item_id: answer.item_id}, 
+//       function(err, res) {
+//       for (var j=0; j<res.length; j++) {
+//         console.log(" Item ID: " + res[j].item_id +
+//         " == Product Name: " + res[j].product_name + 
+//         " == Quantities Left: " + res[j].stock_quantity);
+//        }
+//      }
+//     )
+//   });
+// }
